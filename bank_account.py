481233c0b8176d25d55f40f6c6f0fc5126035635
Abhishek <aasthana.ai@gmail.com>
2025-09-07 01:00:12 +0530

    bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -65,8 +65,38 @@ class SavingAcct(InterestRewardsAcct):
             self.getBalance()
         except BalanceException as error:
             print(f'\nWithdraw interrupted : {error}')
-    
 
+
+
+
+
+
+#inputs to check my code is running properly
+
+Abhishek =BankAccount(1000,"Abhishek")
+Yash =BankAccount(2000,"Yash")
+
+
+Abhishek.getBalance()
+Yash.getBalance()
+
+Yash.deposit(340.92)
+
+Yash.withdraw(10.45)
+Abhishek.transfer(300, Yash)
+
+Aniket=SavingAcct(10203,'Aniket')
+
+Aashish=InterestRewardsAcct(1032,'Aashish')
+
+Aashish.transfer(200,Abhishek)
+
+Aashish.getBalance()
+
+Aniket.deposit(1023)
+
+Aniket.transfer(2310,Yash)
     
         
+
         
